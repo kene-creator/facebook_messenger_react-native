03e3762690ba1d252f373f48054e047786097133
Kene <kayyungx@gmail.com>
2023-05-25 10:38:57 +0100

    setup request file

---
+++ src/shared/request.ts
@@ -0,0 +1,41 @@
+import axios, {InternalAxiosRequestConfig} from 'axios';
+import {Platform} from 'react-native';
+import {Credentials} from './auth/models';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const baseURL =
+  Platform.OS === 'android' ? 'http://10.0.2.2:4000' : 'http://localhost:4000';
+
+const _retrieveConfigCredentials = async (
+  config: InternalAxiosRequestConfig<any>,
+) => {
+  try {
+    const credentials = await AsyncStorage.getItem('credentials');
+
+    if (credentials !== null) {
+      const {token} = JSON.parse(credentials) as Credentials;
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  return config;
+};
+
+const getConfigWithHeaders = async (
+  config: InternalAxiosRequestConfig<any>,
+) => {
+  config.headers['content-type'] = 'application/json';
+  // config.headers['Content-Type'] = 'application/json';
+  return _retrieveConfigCredentials(config);
+};
+
+axios.interceptors.request.use(
+  config => getConfigWithHeaders(config),
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+export const {get, post, put, delete: del} = axios;

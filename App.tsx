cd791852557fcc4607a6018ff406aec2283d01d3
Kene <kayyungx@gmail.com>
2023-05-31 20:57:06 +0100

    add friend provider

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import {Provider as PaperProvider} from 'react-native-paper';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import {AuthProvider} from './src/shared/auth/contexts/auth.context';
+import {FriendProvider} from './src/shared/friends/contexts/friend.context';
 
 import Screens from './src/screens';
 
@@ -10,11 +11,13 @@ function App(): JSX.Element {
   return (
     <QueryClientProvider client={new QueryClient()}>
       <AuthProvider>
-        <SafeAreaProvider>
-          <PaperProvider>
-            <Screens />
-          </PaperProvider>
-        </SafeAreaProvider>
+        <FriendProvider>
+          <SafeAreaProvider>
+            <PaperProvider>
+              <Screens />
+            </PaperProvider>
+          </SafeAreaProvider>
+        </FriendProvider>
       </AuthProvider>
     </QueryClientProvider>
   );
